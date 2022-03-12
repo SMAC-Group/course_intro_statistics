e9ce4c96cd1c5dfd9aac3310e99c1a22eb045fb6
Stéphane Guerrier <stephane.guerrier@unige.ch>
2022-03-12 20:49:40 +0100

    Some more stuff on chapter 1

---
+++ exercises/exc_chap1_diet_1.R
@@ -0,0 +1,12 @@
+# Import data
+library(idar)
+data(diet)
+
+# Compute weight loss
+diet$weight.loss = diet$initial.weight - diet$final.weight
+
+# Select diet
+posw = diet$diet.type=="_"
+
+# Variable of interest
+dietC = diet$weight.loss[____]
---
+++ exercises/solution_chap1_diet_2.R
@@ -0,0 +1,15 @@
+# Import data
+library(idar)
+data(diet)
+
+# Compute weight loss
+diet$weight.loss = diet$initial.weight - diet$final.weight
+
+# Select diet
+posw = diet$diet.type=="C"
+
+# Variable of interest
+dietC = diet$weight.loss[posw]
+
+# Boxplot
+boxplot_w_points(dietC ??????)
